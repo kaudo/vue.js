80ece6024589b1e072fc47bcd81334a08e293ac9
kAUdo <kaudo@msn.com>
2020-07-28 23:14:40 +0900

    HPC19

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Index msg="ㅎㅎㅎㅎㅎㅎㅎp"/>
   </div>
 </template>
 
 <script>
+import Index from './components/Index.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
+    Index,
     HelloWorld
   }
 }
