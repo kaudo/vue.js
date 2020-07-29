d03715b74961dd8e64caaeb0ff8752e2e6645bf0
kaudo <kaudo@msn.com>
2020-07-29 13:30:58 +0900

    LMM18

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <Index msg="ㅎㅎㅎㅎㅎㅎㅎp"/>
+    header<br/>
+    vue.kaudo.com<br/>
+    vue.js sample site<br/>
+
+    <img alt="Vue logo" src="./assets/logo.png" style="width:50px">
+    <Index msg="안녕하세요."/>
+
+    <div style="display:none"><HelloWorld msg="Welcome to Your Vue.js App"/></div>
   </div>
 </template>
 
@@ -20,12 +25,14 @@ export default {
 </script>
 
 <style>
+body{margin:0;padding:0;}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top:0px;
+  background-color:#eee;
 }
 </style>
--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
-    <h1>123 {{ msg }}</h1>
+    <h1>msg {{ msg }}</h1>
+    <button id="btn">클릭이벤트</button>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -35,7 +36,20 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+
+  created(){
+    $('#btn').click(function(){
+      alert('click on created');
+    })
+  },
+
+  mounted(){
+    $('#btn').click(function(){
+      alert('click on mounted');
+    })
   }
+
 }
 </script>
 
