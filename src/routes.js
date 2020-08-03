132f3fa2e510d1ca6bca03469fdd4f163f1ebbea
kAUdo <kaudo@msn.com>
2020-08-04 00:49:47 +0900

    HPC19

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import Login from './components/Login.vue';
 import Student from './components/Student.vue';
 import Redirect from './components/Redirect.vue';
 import Error from './components/404.vue';
-import PartkingLotList from './components/PartkingLotList.vue';
+import ParkingLotList from './components/ParkingLotList.vue';
 import ParkingLotDetail from './components/ParkingLotDetail.vue';
 
 const routes=[
@@ -15,7 +15,7 @@ const routes=[
   {path:'/redirect',component:Redirect,name:'redirect'},
   {path:'/404',component:Error,name:'404'},
 
-  {path:'/parkingLotList',component:PartkingLotList,name:'parkingLotList'},
+  {path:'/parkingLotList',component:ParkingLotList,name:'parkingLotList'},
   {path:'/parkingLotDetail',component:ParkingLotDetail,name:'parkingLotDetail'},
 ];
 
